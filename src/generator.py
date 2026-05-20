314e73740ea766547713ee3081142cf54c3a977b
Brunogatti79 <brunogatti79@icloud.com>
2026-05-19 22:30:26 -0300

    Update generator.py

--- src/generator.py
+++ src/generator.py
@@ -201,7 +201,6 @@ def generate_dashboard(
     else:
         validacion_banner = ""
  
-    import math
     signals_clean    = json.loads(json.dumps(signals, ensure_ascii=False, default=str).replace(': NaN', ': null').replace(':NaN', ':null'))
     signals_json     = json.dumps(signals_clean, ensure_ascii=False)
     index_stats_json = json.dumps(index_stats, ensure_ascii=False, default=str).replace(': NaN', ': null').replace(':NaN', ':null')
